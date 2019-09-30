722cfe50f78f8e71f167b8bf3da4c526ef467b9e
Italalala <itai.almor@yale.edu>
2019-09-30 10:50:07 -0400

    new control systems. partner programmed w/ Sabrina

--- rpiScript.py
+++ rpiScript.py
@@ -16,6 +16,10 @@ enable_osc = True
 # wildcard, iterate
 # amp, shift, wildX, wildY
 
+#resting vals of param control joysticks
+#2675 amp stick
+#2915 shift stick
+
 prevStates = [0, 0, 0, 0, \
 			 0, 0, \
 			 0, 0, 0, 0]
@@ -24,17 +28,19 @@ currStates = [0, 0, 0, 0, \
 			 0, 0, \
 			 0, 0, 0, 0]
 
-# const int TreeButton = 0;
-# const int CarSwitch = 22;
-# const int WaveButton = 21;
-# const int LeafSwitch = 0;
-# const int WildSwitch = 0;
-# const int iterate = 0;
-#
-# const int amp = 25;
-# const int shift = 26;
-# const int wildcardX = 0;
-# const int wildcardY = 0;
+#treeAmp, treeShift, carAmp, carShift, waveAmp, waveShift, leafAmp, leafShift
+synthParams = [[0.5, 1],[0.5, 1],[0.5, 1],[0.5, 1]]
+tree=0
+car=1
+wave=2
+leaf=3
+amp=0
+shift=1
+
+paramChange = 0.1
+
+iterator = -1
+
 
 class SampleListener():
 	labels = ['wave_switch', 'wave_shift', 'wave_volume', \
@@ -43,26 +49,12 @@ class SampleListener():
 	'bird_switch', 'bird_shift', 'bird_volume', \
 	'wildcard_switch', 'wildcard_shift', 'wildcard_volume']
 	#
-	# playBird = 0
-	# playTree = 0
-	# playCar = 0
-	# playWave = 0
-	# playWildcard = 0
-	# volume = 0
-	# currentObj = 0
+
 	osc_client = None
 
 	def add_osc_connect(self, client_connection):
 		self.osc_client = client_connection
 		print(self.osc_client)
-
-	def sendBirdOSC(self, header, content):
-		if enable_osc:
-			msg = OSC.OSCMessage()
-			msg.setAddress("/Birds")
-			for c in content[0:3]:
-				msg.append(c)
-			self.osc_client.send(msg)
 
 	def sendOSC(self, val, content):
 		if enable_osc:
@@ -117,8 +109,9 @@ def main():
 
 			# Send OSC messages for four main sounds
 			for i, val in enumerate(currStates[0:4]):
+				#turn off
 				if val is 0 and prevStates[i] is 1:
-					msg = [0, prevStates[6], prevStates[7]]
+					msg = [0, currStates[6], currStates[7]]
 					listener.sendOSC(i, msg)
 					print(msg)
 
@@ -126,15 +119,36 @@ def main():
 					# just changed states
 					if prevStates[i] is 0:
 						# send message for that state
-						msg = [1, currStates[6], currStates[7]]
+						#turn on
+						iterator=i
+						synthParams[i][amp]=0.5
+						synthParams[i][shift]=1
+						msg = [1, synthParams[i][amp], synthParams[i][shift]
 						listener.sendOSC(i, msg)
 						print(msg)
 					elif currStates[6] is not prevStates[6] or \
 							currStates[7] is not prevStates[7]:
 
-						msg = [2, currStates[6], currStates[7]]
+						#sustain
+						if currStates[6] - prevStates[6] > 1000:
+							synthParams[i][amp] += paramChange
+						elif currStates[6] - prevStates[6] < -1000:
+							synthParams[i][amp] -= paramChange
+
+						if currStates[7] - prevStates[7] > 1000:
+							synthParams[i][shift] += paramChange
+						elif currStates[7] - prevStates[7] < -1000:
+							synthParams[i][shift] -= paramChange
+
+						msg = [2, synthParams[i][amp], synthParams[i][shift]]
 						listener.sendOSC(i, msg)
 						print(msg)
+			if currStates[5] == 1:
+				for i in range(4):
+					iterator = (iterator+1)%4
+					if currStates[iterator] == 1:
+						break
+
 
 
 			# listener.sendBirdOSC("header", currStates)
