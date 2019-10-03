5de1f664fa34d8302f343ea78e06fce17c6f6618
Felicia Chang <felicia.chang@yale.edu>
2019-10-03 18:18:59 -0400

    port change

--- readport.py
+++ readport.py
@@ -2,9 +2,8 @@ from time import sleep
 import random
 import serial # sudo pip install pyserial
 import re
-import gpiozero as gp
 
-ser = serial.Serial('/dev/ttyAMA0', 9600)
+ser = serial.Serial('/dev/cu.SLAB_USBtoUART', 9600)
 
 def main():
     while True:
